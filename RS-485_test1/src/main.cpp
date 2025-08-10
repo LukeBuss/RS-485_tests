13f7e80ac87c1d1388fab309a52f34a56ce26039
Luke B. <PiratePenguin2.2649@gmail.com>
2025-08-09 18:28:34 -0700

    Got blink to work on an nRF

---
+++ RS-485_test1/src/main.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+#include <Adafruit_TinyUSB.h> // Only needed for Seeed nRF52 Boards core
+
+void setup() {
+  Serial.begin(SERIAL_BAUD); // Uses value from platformio.ini
+  while (!Serial) delay(10);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.println("Starting blink test...");
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, LOW); // LED ON
+  Serial.println("LED ON");
+  delay(500);
+
+  digitalWrite(LED_BUILTIN, HIGH); // LED OFF
+  Serial.println("LED OFF");
+  delay(500);
+}
