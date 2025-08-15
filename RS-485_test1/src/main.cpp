3951bc6b3548737b315027b38ea9b16e4c655c7d
Luke B. <PiratePenguin2.2649@gmail.com>
2025-08-14 18:35:59 -0700

    Added physical LED for pin check

--- RS-485_test1/src/main.cpp
+++ RS-485_test1/src/main.cpp
@@ -4,20 +4,27 @@
   #include <Adafruit_TinyUSB.h> // Only needed for Seeed nRF52 Boards core
 #endif
 
+const int LED_PIN = D9; // D7 = 44, D8 = 7, D9 = 8
+
 void setup() {
   Serial.begin(SERIAL_BAUD); // Uses value from platformio.ini
   while (!Serial) delay(10);
 
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.println("Starting blink test...");
+
+  pinMode(LED_PIN, OUTPUT); // Indicator Light
+  digitalWrite(LED_PIN, LOW); // Start OFF
 }
 
 void loop() {
   digitalWrite(LED_BUILTIN, LOW); // LED ON
+  digitalWrite(LED_PIN, HIGH); // Indicator ON
   Serial.println("LED ON");
   delay(500);
 
   digitalWrite(LED_BUILTIN, HIGH); // LED OFF
+  digitalWrite(LED_PIN, LOW); // Indicator OFF
   Serial.println("LED OFF");
   delay(500);
 }
