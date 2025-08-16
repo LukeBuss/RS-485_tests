b8bacb1da6ac92cfd0c0973ee8542d8008d23fb8
Luke B. <PiratePenguin2.2649@gmail.com>
2025-08-16 12:22:37 -0700

    TX RX WORKS!!!

--- RS-485_test1/src/master/main.cpp
+++ RS-485_test1/src/master/main.cpp
@@ -4,17 +4,34 @@
 #define RX_PIN D2   // Board's RX pad (not used on sender, but required by begin)
 #define TX_PIN D1   // Board's TX pad
 
-void setup() {
+const int LED_PIN = D9; // Indicator Light
+
+void setup() {  
+  pinMode(LED_PIN, OUTPUT); // Indicator Light
+  digitalWrite(LED_PIN, HIGH); // LED ON
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW); // LED ON
+
   Serial.begin(115200);                      // USB CDC
   Serial1.begin(115200, SERIAL_8N1, RX_PIN, TX_PIN); // map pins to UART1
   Serial.println("Sender ready");
-
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop() {
   static uint32_t i = 0;
-  Serial1.printf("PING %lu\n", i++);
+  Serial1.printf("ON %lu\n", i++);
+
+  Serial.printf("LED ON %lu\n", i);
+  digitalWrite(LED_PIN, HIGH); // LED ON
+  digitalWrite(LED_BUILTIN, HIGH); // LED OFF
+  delay(500);
+
+  // Toggle LED state
+  Serial1.printf("OFF\n");
+
+  Serial.printf("LED OFF\n");
+  digitalWrite(LED_PIN, LOW); // LED OFF
+  digitalWrite(LED_BUILTIN, LOW); // LED ON
   delay(500);
 }
--- RS-485_test1/src/slave1/main.cpp
+++ RS-485_test1/src/slave1/main.cpp
@@ -1,21 +1,46 @@
 #include <Arduino.h>
 
 // XIAO ESP32-S3 — Receiver
-#define RX_PIN D2
-#define TX_PIN D1
+#define RX_PIN D2   // Board's RX pad
+#define TX_PIN D1   // Board's TX pad (not used on receiver, but required by begin)
+
+const int LED_PIN = D9; // Indicator Light
+unsigned long timeSinceLastBlink = 0;
 
 void setup() {
+  pinMode(LED_PIN, OUTPUT); // Indicator Light
+  digitalWrite(LED_PIN, HIGH); // LED ON
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW); // LED ON
+
   Serial.begin(115200);                      
   Serial1.begin(115200, SERIAL_8N1, RX_PIN, TX_PIN);
   Serial.println("Receiver ready");
-
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop() {
   while (Serial1.available()) {
-    int c = Serial1.read();
-    Serial.write(c);   // show incoming on USB Serial Monitor
+    String input = "";
+    while (Serial1.available()) {
+      char c = Serial1.read();
+      input += c;
+      delay(2); // small delay to allow buffer to fill
+    }
+    // if the first 3 letters of input are "ON "
+    if (input.startsWith("ON ")) {
+      Serial.printf("ON  %lu\n", input.substring(3).toInt());
+      digitalWrite(LED_PIN, HIGH);
+    } else if (input.startsWith("OFF")) {
+      Serial.println("OFF");
+      Serial.println();
+      digitalWrite(LED_PIN, LOW);
+    } else if (input.length() > 0) {
+      Serial.println(input);
+    }
+  }
+  if (millis() - timeSinceLastBlink > 1000) {
+    timeSinceLastBlink = millis();
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN)); // Toggle LED
   }
 }
